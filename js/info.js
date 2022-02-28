221faf1255ffc90eaa7b4fdcad819835d28e0a3c
PrinceNonso <princelynonso@gmail.com>
2022-02-28 16:49:14 +0100

    added images an

---
+++ js/info.js
@@ -0,0 +1,88 @@
+export const eventList =[
+    {
+        id:1,
+        title:"Lets Cook",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore soluta eum praesentium quos, voluptate doloribus inventore perspiciatis, veniam voluptates animi vero nostrum voluptas, repudiandae enim impedit reprehenderit omnis optio dolor molestiae tempora. Assumenda repudiandae praesentium quibusdam, quos numquam cupiditate odio.",
+        info:new Date(),
+        imgSrc='./images/bg.jpg',
+        author:'John doe'
+    }, 
+    {
+        id:2,
+        title:"Cooking is fun",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore soluta eum praesentium quos, voluptate doloribus inventore perspiciatis, veniam voluptates animi vero nostrum voluptas, repudiandae enim impedit reprehenderit omnis optio dolor molestiae tempora. Assumenda repudiandae praesentium quibusdam, quos numquam cupiditate odio.",
+        info:new Date(),
+        author:'John doe'
+    }, 
+    {
+        id:3,
+        title:"Fighting Life",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore soluta eum praesentium quos, voluptate doloribus inventore perspiciatis, veniam voluptates animi vero nostrum voluptas, repudiandae enim impedit reprehenderit omnis optio dolor molestiae tempora. Assumenda repudiandae praesentium quibusdam, quos numquam cupiditate odio.",
+        info:new Date(),
+        author:'John doe'
+    }, 
+    {
+        id:4,
+        title:"How to be a Developer",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore soluta eum praesentium quos, voluptate doloribus inventore perspiciatis, veniam voluptates animi vero nostrum voluptas, repudiandae enim impedit reprehenderit omnis optio dolor molestiae tempora. Assumenda repudiandae praesentium quibusdam, quos numquam cupiditate odio.",
+        info:new Date(),
+        author:'John doe'
+    }, 
+    {
+        id:5,
+        title:"Fight For Life",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore soluta eum praesentium quos, voluptate doloribus inventore perspiciatis, veniam voluptates animi vero nostrum voluptas, repudiandae enim impedit reprehenderit omnis optio dolor molestiae tempora. Assumenda repudiandae praesentium quibusdam, quos numquam cupiditate odio.",
+        info:new Date(),
+        author:'John doe'
+    }, 
+    {
+        id:6,
+        title:"Time to See",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore soluta eum praesentium quos, voluptate doloribus inventore perspiciatis, veniam voluptates animi vero nostrum voluptas, repudiandae enim impedit reprehenderit omnis optio dolor molestiae tempora. Assumenda repudiandae praesentium quibusdam, quos numquam cupiditate odio.",
+        info:new Date(),
+        author:'John doe'
+    }, 
+    {
+        id:7,
+        title:"My Life is all i have",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore soluta eum praesentium quos, voluptate doloribus inventore perspiciatis, veniam voluptates animi vero nostrum voluptas, repudiandae enim impedit reprehenderit omnis optio dolor molestiae tempora. Assumenda repudiandae praesentium quibusdam, quos numquam cupiditate odio.",
+        info:new Date(),
+        author:'John doe'
+    }, 
+    {
+        id:8,
+        title:"Lets Cook",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore soluta eum praesentium quos, voluptate doloribus inventore perspiciatis, veniam voluptates animi vero nostrum voluptas, repudiandae enim impedit reprehenderit omnis optio dolor molestiae tempora. Assumenda repudiandae praesentium quibusdam, quos numquam cupiditate odio.",
+        info:new Date(),
+        author:'John doe'
+    }, 
+    {
+        id:9,
+        title:"Lets Cook",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore soluta eum praesentium quos, voluptate doloribus inventore perspiciatis, veniam voluptates animi vero nostrum voluptas, repudiandae enim impedit reprehenderit omnis optio dolor molestiae tempora. Assumenda repudiandae praesentium quibusdam, quos numquam cupiditate odio.",
+        info:new Date(),
+        author:'John doe'
+    }, 
+    {
+        id:10,
+        title:"Lets Cook",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore soluta eum praesentium quos, voluptate doloribus inventore perspiciatis, veniam voluptates animi vero nostrum voluptas, repudiandae enim impedit reprehenderit omnis optio dolor molestiae tempora. Assumenda repudiandae praesentium quibusdam, quos numquam cupiditate odio.",
+        info:new Date(),
+        author:'John doe'
+    }, 
+    {
+        id:11,
+        title:"Lets Cook",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore soluta eum praesentium quos, voluptate doloribus inventore perspiciatis, veniam voluptates animi vero nostrum voluptas, repudiandae enim impedit reprehenderit omnis optio dolor molestiae tempora. Assumenda repudiandae praesentium quibusdam, quos numquam cupiditate odio.",
+        info:new Date(),
+        author:'John doe'
+    }, 
+    {
+        id:12,
+        title:"Lets Cook",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore soluta eum praesentium quos, voluptate doloribus inventore perspiciatis, veniam voluptates animi vero nostrum voluptas, repudiandae enim impedit reprehenderit omnis optio dolor molestiae tempora. Assumenda repudiandae praesentium quibusdam, quos numquam cupiditate odio.",
+        info:new Date(),
+        author:'John doe'
+    }, 
+    
+]
